b72e17bcb8556f1740f6b8a20f2a58bcfd663e7e
DrCosha <drcosha@gmail.com>
2023-02-28 12:08:23 +0300

    WiFi settings added

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,13 @@ extern "C" {
 }
 #include <AsyncMqttClient.h>
 
+#define WIFI_SSID "iot_ls"                          // SSID нашей локальной сети  
+#define WIFI_PASSWORD "vvssoft40"                   // пароль к нашей локальной сети
+
+#define MQTT_USER "mqtt_user"                       // имя пользователя для подключения к MQTT серверу
+#define MQTT_PWD "vvssoft40"                        // пароль для подключения к MQTT серверу
+#define MQTT_HOST IPAddress(192, 168, 10, 100)      // адрес нашего Mosquito MQTT сервера
+
 void setup() {
   // put your setup code here, to run once:
 }
