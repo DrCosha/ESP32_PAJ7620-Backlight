c9f6ab77a32de35641e78870dd88945f2e3a2473
DrCosha <drcosha@gmail.com>
2023-02-28 11:16:43 +0300

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,19 @@
+
+
+#include <Arduino.h>
+#include <WiFi.h>
+extern "C" {
+#include "freertos/FreeRTOS.h"
+#include "freertos/timers.h"
+}
+#include <AsyncMqttClient.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
+void setup() {
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
