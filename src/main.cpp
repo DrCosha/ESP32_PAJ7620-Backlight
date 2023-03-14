bfc014c93c44e11ac7d45328d2764a02242cf827
DrCosha <drcosha@gmail.com>
2023-03-14 19:11:48 +0300

    Выкладываю для общего доступа. Из кода убрал внутренние данные wifi сети и mqtt

--- src/main.cpp
+++ src/main.cpp
@@ -33,11 +33,11 @@ extern "C" {
 // ------------------------------------------
 
 
-#define WIFI_SSID "iot_ls"                          // SSID нашей локальной сети  
-#define WIFI_PASSWORD "vvssoft40"                   // пароль к нашей локальной сети
+#define WIFI_SSID "wifi_name"                       // SSID нашей локальной сети  
+#define WIFI_PASSWORD "wifi_pwd"                    // пароль к нашей локальной сети
 #define MQTT_USER "mqtt_user"                       // имя пользователя для подключения к MQTT серверу
-#define MQTT_PWD "vvssoft40"                        // пароль для подключения к MQTT серверу
-#define MQTT_HOST IPAddress(192, 168, 10, 100)      // адрес нашего Mosquito MQTT сервера
+#define MQTT_PWD "mqtt_pwd"                         // пароль для подключения к MQTT серверу
+#define MQTT_HOST IPAddress(192, 168, 1, 100)       // адрес нашего Mosquito MQTT сервера
 #define MQTT_PORT 1883                              // порт нашего Mosquito MQTT сервера
 
 #define LWT_TOPIC   "diy/blm32_kitchen/LWT"         // топик публикации доступности устройства
